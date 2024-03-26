294e0a85e83815f8d93fd5c6e9cb1b26599c91bf
SmigorX <igorjaniak@gmail.com>
2024-03-26 22:20:49 +0100

    Fully operational

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
   "fmt"
   "flag"
   "os"
-  //"io/ioutil"
   "log"
   "strings"
   "strconv"
@@ -45,12 +44,9 @@ func file_read (file_path string, task_list *[]Task) {
     document_lines := strings.Split(string(data), "\n")
     document_lines = document_lines[:len(document_lines)-1]
     for _, line := range document_lines {
-      s := strings.Split(line, ";")
-      var b, _ = strconv.ParseBool(s[1])
-      fmt.Println("Tiru riru")
-      fmt.Println(s[1], b)
-      var new_task = Task{s[0], b}
-      *task_list = append(*task_list, new_task)
+      line_elements := strings.Split(line, ";")
+      status, _ := strconv.ParseBool(line_elements[1]) 
+      *task_list = append(*task_list, Task{line_elements[0], status})
     }
   } else {
     log.Fatal("Error while reading a file")
@@ -58,8 +54,18 @@ func file_read (file_path string, task_list *[]Task) {
 }
 
 
-func file_write (file_path string) {
-
+func file_write (file_path string, task_list *[]Task) {
+  if os.Truncate(file_path, 0) != nil {
+    log.Fatal("Error while truncating a file")
+  }
+  file, err := os.OpenFile(file_path, os.O_WRONLY, 0644)
+  if err != nil {
+    log.Fatal("Error while opening a file")
+  }
+  for _, line := range *task_list {
+    file.WriteString(line.name + ";" + strconv.FormatBool(line.status) + "\n")
+  }
+  defer file.Close()
 }
 
 
@@ -68,25 +74,30 @@ func main () {
   var taskList []Task;
 
   file_check(file_path)
-
   file_read(file_path, &taskList)
 
-  print_elements(taskList)
-//  var name = flag.String("n", "" ,"Name of the task" )
-//  var remove = flag.Int("r", 0, "Remove task with given index")
-//  var status = flag.Int("s", 0, "Change the status of a task")
-//  var out = flag.Bool("p", false, "Print tasks")
+  var name = flag.String("n", "" ,"New task" )
+  var remove = flag.Int("r", -1, "Remove task with given index")
+  var status = flag.Int("s", -1, "Change the status of a task")
+  var out = flag.Bool("p", false, "Print tasks")
 
   flag.Parse()
 
-//  if *name != "" {
-    //var task = Task{id:len(taskList), name: *name,}
-    //taskList = append(taskList, task)
-//  } else if *out {
-//    print_elements(taskList)
-//  } else if *remove != 0 {
-//    taskList = append(taskList[:*remove], taskList[*remove+1:]...)
-//  } else if *status != 0 {
-//    taskList[*status].status = !(taskList[*status].status)  
-//  }
+  if *name != "" {
+      var task = Task{name: *name, status: false}
+      taskList = append(taskList, task)
+  }
+  if *out {
+      fmt.Println("Tasks:")
+      print_elements(taskList)
+  } 
+  if *remove != -1 {
+      taskList = append(taskList[:*remove], taskList[*remove+1:]...)
+  }
+  if *status != -1 {
+      taskList[*status].status = !(taskList[*status].status) 
+  }
+  
+  file_write(file_path, &taskList)
 }
+
